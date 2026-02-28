835fbcfafa2ba047dadbe4369c88e83ab368ede4
Utkarsh Nayan <utkarshnayan007@gmail.com>
2026-03-01 03:06:59 +0530

    Initial Commit

---
+++ DIGIFENCEV1/DIGIFENCEV1App.swift
@@ -0,0 +1,17 @@
+//
+//  DIGIFENCEV1App.swift
+//  DIGIFENCEV1
+//
+//  Created by Utkarsh Nayan on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct DIGIFENCEV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
